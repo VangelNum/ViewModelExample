8b64a273ac66647d4ffcbe662f5c84be24f487b7
VangelNum <vangelnum@gmail.com>
2022-10-30 17:42:32 +0400

    Initial commit

---
+++ app/src/main/java/com/vangelnum/viewmodel/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.vangelnum.viewmodel
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.vangelnum.viewmodel.ui.theme.ViewModelTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val vm = TodoViewModel()
+        super.onCreate(savedInstanceState)
+        setContent {
+            ViewModelTheme {
+                TodoView(vm)
+            }
+        }
+    }
+}
+
+@Composable
+fun TodoView(vm: TodoViewModel) {
+
+    LaunchedEffect(Unit, block = {
+        vm.getTodoList()
+    })
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Row {
+                        Text("Todos")
+                    }
+                })
+        },
+        content = {
+            if (vm.errorMessage.isEmpty()) {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    LazyColumn(modifier = Modifier.fillMaxHeight()) {
+                        items(vm.todoList) { todo ->
+                            Column {
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(16.dp),
+                                    horizontalArrangement = Arrangement.SpaceBetween
+                                ) {
+                                    Box(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(0.dp, 0.dp, 16.dp, 0.dp)
+                                    ) {
+                                        Text(
+                                            todo.title,
+                                            maxLines = 1,
+                                            overflow = TextOverflow.Ellipsis
+                                        )
+                                    }
+                                    Spacer(modifier = Modifier.width(16.dp))
+                                    Checkbox(checked = todo.completed, onCheckedChange = null)
+                                }
+                                Divider()
+                            }
+                        }
+                    }
+                }
+            } else {
+                Text(vm.errorMessage)
+            }
+        }
+    )
+}
